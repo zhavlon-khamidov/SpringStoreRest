629738a82c9327c42377cbd57710c46d6e51d1e6
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2023-04-06 12:43:02 +0600

    prefixed url on controller

--- src/main/java/kg/alatoo/storeapi/controllers/ProductController.java
+++ src/main/java/kg/alatoo/storeapi/controllers/ProductController.java
@@ -9,52 +9,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
+
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping(value = "/api/v1/product")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
 
     private final ProductService productService;
 
-    @PatchMapping("/api/v1/product/{productId}")
-    public ResponseEntity updatePatchById(@PathVariable("productId") UUID productId, @RequestBody Product product) {
-        log.info("Patch product with id " + productId + ": "  + product);
+    @PatchMapping( "{productId}")
+    public ResponseEntity<?> updatePatchById(@PathVariable("productId") UUID productId, @RequestBody Product product) {
+        log.debug("Patch product with id " + productId + ": "  + product);
         productService.patchProductById(productId,product);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping("/api/v1/product")
+    @GetMapping
     public List<Product> allProducts() {
+        System.out.println("Getting all product"); // don't use like this
+        log.debug("Getting all product"); // use like this
         return productService.findAll();
     }
 
-    @GetMapping("/api/v1/product/{productId}")
+    @GetMapping( "{productId}")
     public Product getProductById(@PathVariable("productId") UUID productId) {
         return productService.findById(productId);
     }
 
-    @PostMapping("/api/v1/product")
-    public ResponseEntity createProduct(@RequestBody Product product) {
+    @PostMapping
+    public ResponseEntity<?> createProduct(@RequestBody Product product) {
         Product savedProduct = productService.saveProduct(product);
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add("Location","/api/v1/product/" + savedProduct.getId());
-        return new ResponseEntity(responseHeaders,HttpStatus.CREATED);
+        return new ResponseEntity<>(responseHeaders,HttpStatus.CREATED);
     }
 
-    @PutMapping("/api/v1/product/{productId}")
-    public ResponseEntity updateProduct(@PathVariable("productId") UUID productId, @RequestBody Product product) {
+    @PutMapping("/{productId}")
+    public ResponseEntity<?> updateProduct(@PathVariable("productId") UUID productId, @RequestBody Product product) {
         productService.updateProductById(productId,product);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("/api/v1/product/{productId}")
-    public ResponseEntity deleteProduct(@PathVariable("productId") UUID productId) {
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable("productId") UUID productId) {
         productService.deleteById(productId);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
